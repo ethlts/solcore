e964c9d87455bb5dd39f94dc232f8407600c37b6
Marcin Benke <marcin@benke.org>
2024-08-08 15:16:18 +0200

    yule: separate Options module

--- yule/Main.hs
+++ yule/Main.hs
@@ -6,40 +6,10 @@ import Common.Pretty(Pretty(..), nest, render)
 import TM
 import Translate
 import Language.Yul(wrapInSolFunction, wrapInContract)
-import Options.Applicative
+import Options
 import Control.Monad(when)
 import Data.String(fromString)
 
-data Options = Options
-    { input :: FilePath
-    , contract :: String
-    , output :: FilePath
-    , verbose :: Bool
-    } deriving Show
-
-optionsParser :: Parser Options
-optionsParser = Options
-    <$> argument str
-        ( metavar "FILE"
-        <> help "Input file" )
-    <*> strOption
-        ( long "contract"
-        <> short 'c'
-        <> metavar "NAME"
-        <> help "Contract name"
-        <> value "Output")
-    <*> strOption
-        ( long "output"
-        <> short 'o'
-        <> metavar "FILE"
-        <> help "Output file"
-        <> value "Output.sol")
-    <*> switch
-        ( long "verbose"
-        <> short 'v'
-        <> help "Verbosity level"
-        <> showDefault
-        )
 
 main :: IO ()
 main = do
@@ -61,12 +31,4 @@ main = do
     writeFile (output options) (render doc)
 
 
-
-parseOptions :: IO Options
-parseOptions = execParser opts
-  where
-    opts = info (optionsParser <**> helper)
-      ( fullDesc
-     <> progDesc "Compile a Core program to Yul"
-     <> header "yule - experiments with Yul codegen" )
 
