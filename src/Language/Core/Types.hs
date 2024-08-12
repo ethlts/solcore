63c0fd8154c0fc2ae8b065af11b2e7b5a23908da
Marcin Benke <marcin@benke.org>
2024-08-12 11:39:06 +0200

    Core description

--- src/Language/Core/Types.hs
+++ src/Language/Core/Types.hs
@@ -3,10 +3,10 @@ module Language.Core.Types where
 data Type
     = TWord
     | TBool
-    | TPair Type Type
-    | TSum Type Type
-    | TSumN [Type]
+    | TPair Type Type     -- binary product, e.g. (word * word)
+    | TSum Type Type      -- binary sum, e.g. (unit + word)
+    | TSumN [Type]        -- n-ary sum
     | TFun [Type] Type
     | TUnit
-    | TNamed String Type
+    | TNamed String Type  -- named type, e.g. Option{unit + word}
     deriving (Show)
